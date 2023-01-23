c2c0ee6a2ac8f100f2d32c96ea405debb9015ccd
steph-codes <ogundele370@gmail.com>
2023-01-23 18:16:26 +0100

    Creating CQRS for leaveType Query Part 2

--- HR.LeaveManagement.Appication/MappingProfiles/LeaveTypeProfile.cs
+++ HR.LeaveManagement.Appication/MappingProfiles/LeaveTypeProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
 using HR.LeaveManagement.Domain;
 using System;
 using System.Collections.Generic;
@@ -13,9 +14,12 @@ namespace HR.LeaveManagement.Application.MappingProfiles
     {
         public LeaveTypeProfile()
         {
+          
+            //always map from domain entity to Dto, if you do otherwise then use the reverseMap
+            
             CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
 
-            //CreateMap<LeaveType, LeaveTypeDetailsDto>();
+            CreateMap<LeaveType, LeaveTypeDetailsDto>();
         }
 
     }
