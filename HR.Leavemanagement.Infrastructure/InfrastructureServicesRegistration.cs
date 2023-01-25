024ba71606ce87c52ecfb8c692e360862e082e0c
steph-codes <ogundele370@gmail.com>
2023-01-25 12:33:28 +0100

    Setting Up Infrastructure project and Infrastructure Services Registration

---
+++ HR.Leavemanagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Leavemanagement.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+            return services;
+        }
+    }
