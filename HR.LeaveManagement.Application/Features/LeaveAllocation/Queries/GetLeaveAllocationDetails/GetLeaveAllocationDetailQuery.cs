7e32fa2e1e65f13eb34aa3a7b328cb395ae43f2d
steph-codes <ogundele370@gmail.com>
2023-01-26 22:27:39 +0100

    Implemented LeaveTypes and LeaveAllocation CORS

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocationDetails/GetLeaveAllocationDetailQuery.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries
+{
+    public class GetLeaveAllocationDetailQuery : IRequest<LeaveAllocationDetailsDto>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.MappingProfiles
+{
+    public class LeaveAllocationProfile : Profile
+    {
+        public LeaveAllocationProfile()
+        {
+            //CreateMap<LeaveAllocationDto, LeaveAllocation>().ReverseMap();
+            //CreateMap<LeaveAllocation, LeaveAllocationDetailsDto>();
+            //CreateMap<CreateLeaveAllocationCommand, LeaveAllocation>();
+            //CreateMap<UpdateLeaveAllocationCommand, LeaveAllocation>();
+        }
+    }
+}
