bc20bfd2e8ba89ba8309f1e20624f4b3cf85ba73
steph-codes <ogundele370@gmail.com>
2023-01-29 04:09:38 +0100

    Unit Tests - Folder Structure and Installtion of Dependencies

---
+++ HR.LeaveManagement.Application.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HR.LeaveManagement.Application.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]  //fact attribute shows the method below is a test method
+        public void Test1()
+        {
+
+        }
+    }
+}
