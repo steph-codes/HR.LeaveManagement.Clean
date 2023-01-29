3334d9ec33140dadc5cc58e16c91e73fb9b7e887
steph-codes <ogundele370@gmail.com>
2023-01-29 06:02:00 +0100

    Using NSwag and NSwag Studio to consume API

---
+++ HR.LeaveManagement.Blazor.UI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,12 @@
+﻿namespace HR.LeaveManagement.BlazorUI.Services.Base
+{
+    public class BaseHttpService
+    {
+        private readonly IClient _client;
+
+        public BaseHttpService(IClient client)
+        {
+            this._client = client;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Blazor.UI/Services/Base/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace HR.LeaveManagement.BlazorUI.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get
+            {
+                //injected right from serviceclient
+                return _httpClient;
+            }
+        }
+    }
+}
