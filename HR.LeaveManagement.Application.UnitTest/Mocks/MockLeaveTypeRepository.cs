5cb596b46c7765f90512f6d312f730eb4ee96b15
steph-codes <ogundele370@gmail.com>
2023-01-29 04:21:50 +0100

    Unit Testing App Project, Creating Mock LeaveType repository

--- HR.LeaveManagement.Application.UnitTest/Mocks/MockLeaveTypeRepository.cs
+++ HR.LeaveManagement.Application.UnitTest/Mocks/MockLeaveTypeRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,46 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.UnitTest.Mocks
 {
-    internal class MockLeaveTypeRepository
+    public  class MockLeaveTypeRepository
     {
+        public static Mock<ILeaveTypeRepository> GetMockLeaveTypeRepository()
+        {
+            //create a control List of what the actual data should look like
+            var leaveTypes = new List<LeaveType>
+            {
+                new LeaveType
+                {
+                    Id = 1,
+                    DefaultDays = 10,
+                    Name = "Test Vacation"
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    DefaultDays = 15,
+                    Name = "Test Sick"
+                },
+                new LeaveType
+                {
+                    Id = 3,
+                    DefaultDays = 15,
+                    Name = "Test Maternity"
+                }
+            };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+
+            mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
+
+            //It.IsAny is saying that LeaveType Obj is the param needed to be parsed 
+            mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+                .Returns((LeaveType leaveType) =>
+                {
+                    leaveTypes.Add(leaveType);
+                    return Task.CompletedTask;
+                });
+
+            return mockRepo;
+        }
     }
 }
